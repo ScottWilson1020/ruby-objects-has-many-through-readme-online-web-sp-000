9cf80f1433fff75f9a17c02d5d2d209325fccc94
Scott Wilson <scottwilson1020@gmail.com>
2021-01-25 21:07:25 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -19,4 +19,11 @@ def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
   end
 
+def meals
+  Meal.all.select do |meal|
+    meal.waiter == self #checking for waiter now
+  end
+end
+
+
 end
